1ac5ec3a426f8c8fc139eb59a685578323428c3a
Andy Smith <andysmith836@gmail.com>
2019-07-05 21:38:28 +0100

    Convert to vector

--- src/minesweeper_clj/core.clj
+++ src/minesweeper_clj/core.clj
@@ -1,10 +1,11 @@
 (ns minesweeper-clj.core
   (:gen-class))
 
+
 (defn get-mines [grid]
-  (if (= (nth grid 0) "*")
+  [(if (= (nth grid 0) "*")
     "*"
-    "0"))
+    "0")])
 
 (defn -main
   "I don't do a whole lot ... yet."
--- test/minesweeper_clj/core_test.clj
+++ test/minesweeper_clj/core_test.clj
@@ -4,9 +4,14 @@
 
 (deftest single-item
   (testing "Single item"
-    (is (= "0" (get-mines ["."])))))
+    (is (= ["0"] (get-mines ["."])))))
 
 (deftest single-mine
   (testing "Single mine"
-    (is (= "*" (get-mines ["*"])))))
+    (is (= ["*"] (get-mines ["*"])))))
+
+;(deftest two-cells
+;  (testing "2 cells"
+;    (is (= "*" (get-mines ["*" "."])))))
+
 

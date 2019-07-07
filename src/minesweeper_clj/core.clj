85b3a3da9f7383a13517d8f51e1a3b635277feb5
Andy Smith <andysmith836@gmail.com>
2019-07-07 22:11:37 +0100

    Write function to count a line of mines:

--- src/minesweeper_clj/core.clj
+++ src/minesweeper_clj/core.clj
@@ -2,12 +2,37 @@
   (:gen-class))
 
 
-(defn get-mines [grid]
-  [(if (= (nth grid 0) "*")
-    "*"
-    "0")])
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn count-mine [cell]
+  (if (= cell "*")
+    1
+    0))
+
+
+
+(defn add-up [left right] (+ left (count-mine right)))
+
+(defn count-row [row]
+  (reduce (fn [left right] (add-up left right)) 0 row)
+  )
+
+
+
+
+
+
+(defn get-mines [grid]
+  [(count-mine (nth grid 0))]
+  )
+
+
+
+
+;(defn count-mines [grid]
+;  (def list (loop [i 0]
+;    (when (< i (count grid))
+;      (count-mine grid i)
+;    (recur (+ i 1)))
+;  ))
+;  (println "list is " (count list))
+;  )
--- test/minesweeper_clj/core_test.clj
+++ test/minesweeper_clj/core_test.clj
@@ -2,16 +2,20 @@
   (:require [clojure.test :refer :all]
             [minesweeper-clj.core :refer :all]))
 
-(deftest single-item
-  (testing "Single item"
-    (is (= ["0"] (get-mines ["."])))))
-
-(deftest single-mine
-  (testing "Single mine"
-    (is (= ["*"] (get-mines ["*"])))))
+;(deftest single-item
+;  (testing "Single item"
+;    (is (= ["0"] (get-mines ["."])))))
+;
+;(deftest single-mine
+;  (testing "Single mine"
+;    (is (= ["*"] (get-mines ["*"])))))
 
 ;(deftest two-cells
 ;  (testing "2 cells"
-;    (is (= "*" (get-mines ["*" "."])))))
+;    (is (= ["0" "0"] (get-mines ["." "."])))))
 
 
+
+(deftest test-count-row
+  (testing "a row is summed"
+    (is (= 2 (count-row ["." "*" "*"])))))

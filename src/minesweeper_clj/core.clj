2a7c5681d18ef8dbb82f18d0595719b04dbc346b
Andy Smith <andysmith836@gmail.com>
2019-07-09 22:00:32 +0100

    Got counting of row working

--- src/minesweeper_clj/core.clj
+++ src/minesweeper_clj/core.clj
@@ -1,20 +1,27 @@
 (ns minesweeper-clj.core
   (:gen-class))
 
-
-
 (defn count-mine [cell]
   (if (= cell "*")
     1
     0))
 
 
+(defn count-around-cell [row range]
+  (count (filter (fn [i] (= (nth row i) "*")) range)))
 
-(defn add-up [left right] (+ left (count-mine right)))
+(defn cropped-cells [row x]
+  (filter
+    (fn [i]
+      (and (> i -1)
+           (< i (count row)))
+      )
+    (range (- x 1) (+ x 2))))
 
-(defn count-row [row]
-  (reduce (fn [left right] (add-up left right)) 0 row)
-  )
+(defn count-row [row x]
+  (count-around-cell row (cropped-cells row x)))
+
+
 
 (defn row-above [rows y]
   (if (> y 0)
@@ -24,13 +31,16 @@
   (if (< y (- (count rows) 1))
     (nth rows (+ y 1)) []))
 
+(defn cell-left [row x]
+  (if (> x 0)
+    (nth row (- x 1)) 0))
 
 (defn count-pos [rows x y]
-  (+ (count-row (row-above rows y))
-     (count-mine (nth (nth rows y) (- x 1)))
+  (+ (count-row (row-above rows y) x)
+     (count-mine (cell-left (nth rows y) x))
      (count-mine (nth (nth rows y) (+ x 1)))
-     (count-row (row-below rows y))
-  ))
+     (count-row (row-below rows y) x)
+     ))
 
 
 
--- test/minesweeper_clj/core_test.clj
+++ test/minesweeper_clj/core_test.clj
@@ -27,28 +27,45 @@
 ;                         ["." "*" "*"]
 ;                         ["*" "." "*"]] 1 1)))))
 
-(deftest test-mine-in-the-middle
-  (testing "3 rows"
-    (is (= 6 (count-pos [
-                         ["." "*" "*"]
-                         ["*" "." "*"]
-                         ["*" "*" "."]] 1 1)))))
+;(deftest test-mine-in-the-middle
+;  (testing "3 rows"
+;    (is (= 6 (count-pos [
+;                         ["." "*" "*"]
+;                         ["*" "." "*"]
+;                         ["*" "*" "."]] 1 1)))))
+;
+;(deftest test-dont-count-above
+;(testing "dont count above"
+;  (is (= 3 (count-pos [
+;                       ["." "*" "*"]
+;                       ["*" "." "*"]
+;                       ["*" "*" "."]] 1 0)))))
+;
+;(deftest test-dont-count-below
+;  (testing "dont count below"
+;    (is (= 3 (count-pos [
+;                         ["." "*" "*"]
+;                         ["*" "." "*"]
+;                         ["*" "." "."]] 1 2)))))
 
-(deftest test-dont-count-above
-(testing "dont count above"
-  (is (= 3 (count-pos [
-                       ["." "*" "*"]
-                       ["*" "." "*"]
-                       ["*" "*" "."]] 1 0)))))
-
-(deftest test-dont-count-below
-  (testing "dont count below"
-    (is (= 3 (count-pos [
-                         ["." "*" "*"]
-                         ["*" "." "*"]
-                         ["*" "." "."]] 1 2)))))
+;(deftest test-dont-count-left
+;(testing "left bounds"
+;  (is (= 2 (count-pos [
+;                       ["." "*" "*"]
+;                       ["." "." "*"]
+;                       ["*" "." "."]] 0 1)))))
 
 
 (deftest test-count-row
   (testing "a row is summed"
-    (is (= 2 (count-row ["." "*" "*"])))))
+    (is (= 1 (count-row ["." "*" "*"] 0)))
+    (is (= 2 (count-row ["." "*" "*"] 1)))
+    (is (= 2 (count-row ["." "*" "*"] 2)))
+    ))
+
+
+(deftest test-area-around-cell
+  (testing "we get cells relevant"
+    (is (= [0 1 2] (cropped-cells [0 1 2] 1)))
+    (is (= [0 1] (cropped-cells [0 1 2] 0)))
+    (is (= [1 2] (cropped-cells [0 1 2] 2)))))
